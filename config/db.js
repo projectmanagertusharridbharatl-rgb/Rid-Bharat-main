16ab2185159700822c8d3118fdbb202fc24edefe
Tushar Lilhare <projectmanagertusharridbharatl@gmail.com>
2025-12-28 18:45:38 +0530

    init

--- config/db.js
+++ config/db.js
@@ -1,32 +1,19 @@
-// const mongoose = require('mongoose');
-// require('dotenv').config(); // Load .env variables
-
-// const connectDB = async () => {
-//     try {
-//        await mongoose.connect('mongodb+srv://subhaskushwahak:subhas%401234@cluster0.v5vwpbb.mongodb.net/');
-//         // await mongoose.connect('mongodb+srv://aayushpawar7748:9kt2pwbJ1u8hVTVZ@cluster0.zprlwo7.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0');
-
-//         //await mongoose.connect('mongodb+srv://pawarharsh191:harsh123@cluster0.bq00w.mongodb.net/ProjectRid');
-//         // await mongoose.connect('mongodb+srv://pawarharsh191:harsh123@cluster0.bq00w.mongodb.net/ProjectRid');
-//       console.log("Mongodb connection successful");
-//     } catch (error) {
-//         console.error("MongoDB connection error:", error);
-//         process.exit(1);
-//     }
-// };
-
-// module.exports = connectDB;
 const mongoose = require("mongoose");
+require("dotenv").config();
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGO_URI
-);
-    console.log("MongoDB connected successfully!");
+    const uri = process.env.MONGO_URI;
+
+    console.log("Mongo URI loaded:", uri ? "YES" : "NO");
+
+    await mongoose.connect(uri);
+
+    console.log("✅ MongoDB Connected");
   } catch (error) {
-    console.log("❌ MongoDB connection error:", error);
+    console.error("❌ MongoDB connection error:", error.message);
+    process.exit(1);
   }
 };
 
 module.exports = connectDB;
-
