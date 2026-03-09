7815218116ae9aa9dbf20d0e0372c5d3b5bda1c9
Deepakkumarsah25 <deepak232a@gmail.com>
2026-03-09 22:08:16 +1100

    update rts cards

--- routes/applicationRoutes.js
+++ routes/applicationRoutes.js
@@ -5,12 +5,6 @@ const controller = require("../controllers/applicationController");
 // Apply for certificate
 router.post("/apply", controller.applyCertificate);
 
-// User verification
-router.get("/verify/:appId", controller.verifyById);
-
-// Admin verification (from email link)
-router.get("/verify-admin/:appId", controller.adminVerify);
-
 // Download certificate
 router.get("/download/:appId", controller.downloadCertificate);
 
